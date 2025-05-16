b4499570b37afad8c83a45223683554cfe9ffabc
rumiaktar <ce.rumiakter@gmail.com>
2025-05-16 21:29:53 +0600

    project setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../Root/Root";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root></Root>,
+        children:[
+            {
+                path:'/',
+                
+            }
+        ]
+    },
+]);
+
+export default router;
---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <h1>This is Fotter</h1>
+        </div>
+    );
+};
+
+export default Footer;
