2a90e0db4930f2cc4fd105d6ef79ed034f777931
rumiaktar <ce.rumiakter@gmail.com>
2025-06-23 23:45:42 +0600

    add all experts

--- src/pages/Home/Experts/Expert.jsx
+++ src/pages/Home/Experts/Expert.jsx
@@ -1,9 +1,23 @@
 
 
-const Expert = () => {
+const Expert = ({ expert }) => {
+    const { img, title, designation } = expert;
     return (
         <div>
-            
+            <div className="card bg-base-100 shadow-sm">
+                <figure>
+                    <img className="w-full"
+                        src={img}
+                        alt="Shoes" />
+                </figure>
+                <div className="card-body text-center">
+                    <h2 className="card-title mx-auto">{title}</h2>
+                    <p>{designation}</p>
+                    <div className="card-actions ">
+                        <button className="btn  bg-linear-to-bl from-[#C049A799] to-[#6C3CE999] text-white w-full">View Details</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Home/Experts/Experts.jsx
+++ src/pages/Home/Experts/Experts.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
+import Expert from "./Expert";
 
 
 const Experts = () => {
+    const [experts, setExperts] = useState([]);
+
+    useEffect(() =>{
+        fetch("experts.json")
+        .then(res => res.json())
+        .then(data => setExperts(data))
+    } ,[])
     return (
-        <div>
-            
+        <div className="container mx-auto mb-16">
+            <h1 class="text-2xl font-medium text-center mb-8">Our Services</h1>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+                {
+                    experts.map((expert, idx) =><Expert key={idx} expert = {expert}></Expert>)
+                }
+            </div>
         </div>
     );
 };
