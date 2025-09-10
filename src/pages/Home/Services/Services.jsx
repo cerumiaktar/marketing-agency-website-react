e13c2b1e38f5b6d9d7fd53e4748c0a7bb4b3b599
Rumi Aktar <ce.rumiakter@gmail.com>
2025-09-10 23:14:27 +0600

    update

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -12,7 +12,7 @@ const Services = () => {
     } ,[]);
     return (
         <div className="container mx-auto mt-20 mb-20">
-            <h1 class="text-2xl font-medium text-center mb-8">Our Services</h1>
+            <h1 class="text-2xl font-medium text-center mb-8 dark:text-black">Our Services</h1>
             <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
                 {
                     services.map(service =><Service service ={service}></Service>)
