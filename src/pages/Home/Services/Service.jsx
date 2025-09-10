b40a425fa047220eb9bcc5b77ee326602a96e454
Rumi Aktar <ce.rumiakter@gmail.com>
2025-09-10 23:16:43 +0600

    update services section

--- src/pages/Home/Services/Service.jsx
+++ src/pages/Home/Services/Service.jsx
@@ -4,17 +4,17 @@ const Service = ({ service }) => {
     const { img, title, short_description } = service;
     return (
         <div>
-            <div className="card bg-base-100 border border-[#6C3CE980] shadow-2xl py-4">
+            <div className="card bg-base-100 dark:bg-white border border-[#6C3CE980] shadow-2xl py-4">
                 <figure>
                     <img
                         src={img}
                         alt="Services" />
                 </figure>
                 <div className="card-body text-center">
-                    <h2 className="card-title mx-auto">{title}</h2>
+                    <h2 className="card-title mx-auto dark:text-black">{title}</h2>
                     <p className="md:w-8/12 mx-auto text-[#403D3D]">{short_description}</p>
                     <div className="card-actions">
-                        <button className="btn bg-linear-to-bl from-[#C049A799] to-[#6C3CE999] text-white w-full">Buy Now</button>
+                        <button className="btn bg-linear-to-bl from-[#C049A799] to-[#6C3CE999] text-white w-full dark:border-none">Buy Now</button>
                     </div>
                 </div>
             </div>
