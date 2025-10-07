2b703e37b4d100e3e1cae0265a9254a709374b10
unknown <ce.rumiakter@gmail.com>
2025-10-07 11:27:05 +0600

    update

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../pages/Shared/Navbar/Navbar";
 
 
 const Root = () => {
     return (
         <div className=" dark:bg-white">
+            <Navbar></Navbar>
             <div className="min-h-screen">
                 <Outlet></Outlet>
             </div>
