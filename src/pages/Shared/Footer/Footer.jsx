bf14683fb1360b18a11e1027bd6d2b9a062256e7
unknown <ce.rumiakter@gmail.com>
2025-10-08 12:25:48 +0600

    update

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import { FaFacebook,FaInstagramSquare } from "react-icons/fa";
+import { FaFacebook,FaInstagramSquare,FaLinkedin,FaYoutubeSquare } from "react-icons/fa";
 
 
 const Footer = () => {
@@ -15,17 +15,8 @@ const Footer = () => {
                     <div className="grid grid-flow-col gap-4">
                         <a className="text-2xl"><FaFacebook /></a>
                         <a className="text-2xl"><FaInstagramSquare /></a>
-                        <a>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                className="fill-current">
-                                <path
-                                    d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-                            </svg>
-                        </a>
+                        <a className="text-2xl"><FaLinkedin /></a>
+                        <a className="text-2xl"><FaYoutubeSquare /></a>
                     </div>
                 </nav>
                 <aside>
