fa05f9b7f6143a6c9831d877c10c54f2c28d7f39
unknown <ce.rumiakter@gmail.com>
2025-10-14 12:22:09 +0600

    update

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -5,17 +5,20 @@ import Service from "./Service";
 const Services = () => {
     const [services, setServices] = useState([]);
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch("services.json")
-        .then(res => res.json())
-        .then(data =>setServices(data))
-    } ,[]);
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, []);
     return (
         <div className="container mx-auto mt-20 mb-20">
-            <h1 class="text-2xl font-medium text-center mb-8 dark:text-black">Our Services</h1>
+            <div className="flex items-center justify-between">
+                <h1 class="text-2xl font-medium mb-8 dark:text-black">Our Services</h1>
+                <p className="text-lg">See all</p>
+            </div>
             <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
                 {
-                    services.map(service =><Service service ={service}></Service>)
+                    services.map(service => <Service service={service}></Service>)
                 }
             </div>
         </div>
