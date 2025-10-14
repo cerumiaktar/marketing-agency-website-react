6360cf7a0ef8e506bc0369ec051f58595db95c90
unknown <ce.rumiakter@gmail.com>
2025-10-14 12:20:44 +0600

    update

--- src/pages/Home/Experts/Experts.jsx
+++ src/pages/Home/Experts/Experts.jsx
@@ -5,21 +5,24 @@ import Expert from "./Expert";
 const Experts = () => {
     const [experts, setExperts] = useState([]);
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch("experts.json")
-        .then(res => res.json())
-        .then(data => setExperts(data))
-    } ,[])
+            .then(res => res.json())
+            .then(data => setExperts(data))
+    }, [])
     return (
         <div className="container mx-auto">
-            <h1 class="text-2xl font-medium text-center mb-8 dark:text-black">Our Experts</h1>
+            <div className="flex items-center justify-between">
+                <h1 class="text-2xl font-medium mb-8 dark:text-black">Our Experts</h1>
+                <p className="text-lg">See all</p>
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
                 {
-                    experts.map((expert, idx) =><Expert key={idx} expert = {expert}></Expert>)
+                    experts.map((expert, idx) => <Expert key={idx} expert={expert}></Expert>)
                 }
             </div>
         </div>
-        
+
     );
 };
 
