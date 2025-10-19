278e6ff4cf2d167a3269b6f7547c21b47a25bc33
unknown <ce.rumiakter@gmail.com>
2025-10-19 11:27:03 +0600

    update

--- src/pages/Home/Blogs/Blogs.jsx
+++ src/pages/Home/Blogs/Blogs.jsx
@@ -11,12 +11,12 @@ const Blogs = () => {
         .then(data=>setBlogs(data))
     } ,[])
     return (
-        <div className="container mx-auto">
+        <div className="container mx-auto mb-20">
             <div className="flex items-center justify-between">
                 <h1 class="text-2xl font-medium mb-8 dark:text-black">Blogs</h1>
                 <p className="text-lg">See all</p>
             </div>
-            <div className="">
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
                 {
                     blogs.map((blog)=><Blog blog={blog}></Blog>)
                 }
