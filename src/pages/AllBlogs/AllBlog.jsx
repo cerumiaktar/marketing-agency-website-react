ce5fddf9b89b6eff363411abf322a1c234f365e5
unknown <ce.rumiakter@gmail.com>
2025-10-20 18:29:02 +0600

    update

--- src/pages/AllBlogs/AllBlog.jsx
+++ src/pages/AllBlogs/AllBlog.jsx
@@ -1,9 +1,27 @@
+import { Link } from "react-router-dom";
 
 
-const AllBlog = () => {
+const AllBlog = ({ allBlog }) => {
+    const { id, img, title, description } = allBlog;
     return (
         <div>
-            
+            <div className="flex grow">
+                <div className="card bg-base-100 dark:bg-white shadow-sm">
+                    <figure>
+                        <img
+                            className="w-full"
+                            src={img}
+                            alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title dark:text-black">{title}</h2>
+                        <p className="dark:text-[#333333]">{description}</p>
+                        <div className="w-full">
+                            <Link><button className="btn bg-linear-to-bl from-[#C049A799] to-[#6C3CE999] dark:border-none text-white w-full">Read More</button></Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/AllBlogs/AllBlogs.jsx
+++ src/pages/AllBlogs/AllBlogs.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
+import AllBlog from "./AllBlog";
 
 
 const AllBlogs = () => {
+    const [allBlogs, setAllBlogs] = useState([])
+
+    useEffect(() =>{
+        fetch('blogs.json')
+        .then(res =>res.json())
+        .then(data =>setAllBlogs(data))
+    }, [])
     return (
-        <div>
-            
+        <div className="container mx-auto">
+            <div className="">
+                {
+                    allBlogs.map((allBlog) =><AllBlog allBlog = {allBlog}></AllBlog>)
+                }
+            </div>
         </div>
     );
 };
