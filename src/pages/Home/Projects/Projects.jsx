61a828c4add74341626d96e6346d9d10af0bf541
unknown <ce.rumiakter@gmail.com>
2025-11-08 17:52:51 +0600

    update

--- src/pages/Home/Projects/Projects.jsx
+++ src/pages/Home/Projects/Projects.jsx
@@ -1,9 +1,12 @@
 
 
 const Projects = () => {
+
     return (
         <div>
-            
+            <div className="text-center">
+                <h1 class="text-2xl font-bold mb-8 text-white">Our Client Review</h1>
+            </div>
         </div>
     );
 };
