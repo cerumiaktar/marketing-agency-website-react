c4bda33fb0e93976fd9362dc871d4c2d618d7b4c
unknown <ce.rumiakter@gmail.com>
2025-11-12 16:03:31 +0600

    update package page

--- src/pages/Home/Packages/Packages.jsx
+++ src/pages/Home/Packages/Packages.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
+import Package from "./Package";
 
 const Packages = () => {
+    const [packages, setPackages] = useState([])
+    
+    useEffect(() =>{
+        fetch('packages.json')
+        .then(res=>res.json())
+        .then(data=>setPackages(data))
+    } ,[])
     return (
-        <div>
-            
+        <div className="bg-[#C049A740] py-20 mb-12">
+            <div className="text-center">
+                <h1 class="text-3xl font-bold mb-8">Pricing</h1>
+            </div>
+            <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-6">
+                {
+                    packages.map((pric) =><Package pric ={pric}></Package>)
+                }
+            </div>
         </div>
     );
 };
