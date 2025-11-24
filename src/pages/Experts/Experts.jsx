70edd2bf14424304c4fe226bbc2c8089f3355350
unknown <ce.rumiakter@gmail.com>
2025-11-24 18:05:36 +0600

    update

--- src/pages/Experts/Experts.jsx
+++ src/pages/Experts/Experts.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Expert from './Expert';
 
 const Experts = () => {
+    const [experts, setExperts] = useState([]);
+
+    useEffect(() => {
+        fetch("experts.json")
+            .then(res => res.json())
+            .then(data => setExperts(data))
+    }, [])
     return (
         <div>
-            
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+                {
+                    experts.map((expert, idx) => <Expert key={idx} expert={expert}></Expert>)
+                }
+            </div>
         </div>
     );
 };
