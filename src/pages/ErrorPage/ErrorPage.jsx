ab33b05e434038efc4b6a87e13f392bf51b27f29
unknown <ce.rumiakter@gmail.com>
2025-11-26 18:32:07 +0600

    update

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,12 +1,15 @@
 import { FaArrowRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
         <div className="container mx-auto mt-20">
             <img className="mx-auto" src="https://i.ibb.co.com/hx6gnPSw/Frame-removebg-preview.png" alt="" />
-            <div className="text-center mt-12">
-                <button className="btn bg-red-700 text-white">Home Page <FaArrowRight /></button>
-            </div>
+            <Link>
+                <div className="text-center mt-12">
+                    <button className="btn bg-red-700 text-white">Home Page <FaArrowRight /></button>
+                </div>
+            </Link>
         </div>
     );
 };
