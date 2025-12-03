a7756eb6c0ee1d3a53bbb66d49175d21c4168a57
unknown <ce.rumiakter@gmail.com>
2025-12-03 16:10:43 +0600

    complete login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,29 @@
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
     return (
         <div>
-            <h1>This is login page</h1>
+            <div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content flex-col md:w-3xl">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Login now</h1>
+                    </div>
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <div className="card-body">
+                            <fieldset className="fieldset">
+                                <label className="label">Email</label>
+                                <input type="email" className="input" placeholder="Email" />
+                                <label className="label">Password</label>
+                                <input type="password" className="input" placeholder="Password" />
+                                <div><a className="link link-hover">Forgot password?</a></div>
+                                <button className="btn bg-linear-to-bl from-[#C049A799] to-[#6C3CE999] dark:border-none text-white mt-4">Login</button>
+                            </fieldset>
+                            <p className="text-center">Do not have any acoount? <Link to='/signup' className="font-bold text-[#6C3CE9]">Sign up</Link></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
