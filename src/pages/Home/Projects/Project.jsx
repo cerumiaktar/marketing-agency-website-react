58693eb02af6fba25e2cb5ffe7defd46591b46d0
unknown <ce.rumiakter@gmail.com>
2025-12-03 11:58:59 +0600

    update

--- src/pages/Home/Projects/Project.jsx
+++ src/pages/Home/Projects/Project.jsx
@@ -5,7 +5,7 @@ const Project = ({ project }) => {
     const { img, title } = project;
     return (
         <div>
-            <div className="card bg-base-100 shadow-2xl border border-[#C049A733]">
+            <div className="card bg-base-100 dark:bg-white shadow-2xl border border-[#C049A733] dark:border-none">
                 <figure>
                     <img
                         className="w-full"
@@ -13,7 +13,7 @@ const Project = ({ project }) => {
                         alt="Shoes" />
                 </figure>
                 <div className="card-body">
-                    <h2 className="card-title mx-auto">{title}</h2>
+                    <h2 className="card-title mx-auto dark:text-black">{title}</h2>
                     <Link to='/contact'>
                         <div className="card-actions">
                             <button className="btn bg-linear-to-bl from-[#C049A799] to-[#6C3CE999] dark:border-none text-white w-full">Contact</button>
