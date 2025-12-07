7158e0563bfe4d1f71dc2b5b16b7ce1f850cbab0
unknown <ce.rumiakter@gmail.com>
2025-12-07 12:11:55 +0600

    add contact banner

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -2,8 +2,21 @@
 
 const Contact = () => {
     return (
-        <div>
-            
+        <div className="container mx-auto mt-12 mb-12">
+            <div
+                className="hero h-[200px] mb-12 rounded-xl"
+                style={{
+                    backgroundImage:
+                        "url(https://i.ibb.co.com/5hmn8KLq/Rectangle-4.png)",
+                }}
+            >
+                <div className="hero-overlay bg-[#C049A7C7] rounded-xl"></div>
+                <div className="hero-content text-neutral-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">Contact Us</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
